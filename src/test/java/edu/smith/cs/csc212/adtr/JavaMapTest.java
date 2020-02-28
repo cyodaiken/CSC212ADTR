b1d55b621b1b94b6b4a04b71aea8e991c557b273
cyodaiken <43050529+cyodaiken@users.noreply.github.com>
2020-02-28 14:42:27 -0500

    on ListADT

--- src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
@@ -19,6 +19,99 @@ public class JavaMapTest {
 	public void testEmpty() {
 		MapADT<String, String> empty = new JavaMap<>();
 		assertEquals(empty.size(), 0);
-		//Assert.assert
+		// Assert.assert
 	}
+	
+	@Test
+	public void testInsert() {
+		MapADT<String, String> map1 = new JavaMap<>();
+		map1.put("A", "Aardvark");
+		assertEquals(map1.size(), 1 );
+		// Assert.assert
+	}
+	
+	@Test
+	public void testInsert2() {
+		MapADT<String, String> map2 = new JavaMap<>();
+		map2.put("A", "Aardvark");
+		map2.put("C", "Cat");
+		map2.put("K", "Cat");
+		assertEquals(map2.size(), 3);
+		assertEquals(map2.get("A"), "Aardvark");
+		assertEquals(map2.get("C"), "Cat");
+		assertEquals(map2.get("K"), "Cat");
+		
+		// Assert.assert
+	}
+	
+	@Test
+	public void testInsert3() {
+		MapADT<String, String> map3 = new JavaMap<>();
+		map3.put("A", "Aardvark");
+		map3.put("C", "Cat");
+		assertEquals(map3.size(), 2);
+		map3.put("C", "Cat");
+		assertEquals(map3.size(), 2);
+		// Assert.assert
+	}
+	
+	@Test
+	public void testInsert4() {
+		MapADT<String, String> map3 = new JavaMap<>();
+		map3.put("A", "Aardvark");
+		map3.put("C", "Cat");
+		assertEquals(map3.size(), 2);
+		assertEquals(map3.get("C"), "Cat");
+		map3.put("C", "Cow");
+		assertEquals(map3.size(), 2);
+		assertEquals(map3.get("C"), "Cow");
+		// Assert.assert
+	}
+	
+	@Test
+	public void testGet() {
+		MapADT<String, String> map4 = new JavaMap<>();
+		map4.put("A", "Aardvark");
+		map4.put("C", "Cat");
+		assertEquals(map4.get("A"), "Aardvark");
+			
+	}
+	
+	@Test
+	public void testRepeatedGet() {
+		MapADT<String, String> map5 = new JavaMap<>();
+		map5.put("A", "Aardvark");
+		map5.put("C", "Cat");
+		assertEquals(map5.get("D"), null);		
+	}
+	
+	@Test
+	public void testSize() {
+		MapADT<String, String> map6 = new JavaMap<>();
+		map6.put("A", "Aardvark");
+		map6.put("C", "Cat");
+		assertEquals(map6.size(), 2);		
+	}
+	
+	@Test
+	public void testRemove() {
+		MapADT<String, String> map7 = new JavaMap<>();
+		map7.put("A", "Aardvark");
+		map7.put("C", "Cat");
+		map7.remove("C");
+		assertEquals(map7.size(), 1);
+		assertEquals(map7.get("C"), null);
+	}
+	
+	@Test
+	public void testRepeatedRemove() {
+		MapADT<String, String> map8 = new JavaMap<>();
+		map8.put("A", "Aardvark");
+		map8.put("C", "Cat");
+		map8.remove("Z");
+		assertEquals(map8.size(), 2);
+		assertEquals(map8.get("A"), "Aardvark");
+		assertEquals(map8.get("C"), "Cat");
+	}
+	
 }
--- src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
@@ -14,4 +14,69 @@ public class JavaSetTest {
 		assertEquals(empty.size(), 0);
 	}
 	
+	@Test
+	public void testInsert1() {
+		SetADT<String> set1 = new JavaSet<>();
+		set1.insert("A");
+		assertEquals(set1.size(), 1);	
+	}
+	
+	@Test
+	public void testInsert2() {
+		SetADT<String> set2 = new JavaSet<>();
+		set2.insert("A");
+		set2.insert("B");
+		assertEquals(set2.size(), 2);	
+	}
+	
+	@Test
+	public void testInsert() {
+		SetADT<String> set3 = new JavaSet<>();
+		set3.insert("A");
+		set3.insert("A");
+		assertEquals(set3.size(), 1);	
+	}
+	
+	@Test
+	public void testContains() {
+		SetADT<String> set4 = new JavaSet<>();
+		set4.insert("A");
+		set4.insert("C");
+		assertEquals(set4.contains("A"), true);	
+		assertEquals(set4.contains("C"), true);
+	}
+	
+	@Test
+	public void testNotContains() {
+		SetADT<String> set5 = new JavaSet<>();
+		set5.insert("A");
+		set5.insert("C");
+		assertEquals(set5.contains("Z"), false);
+		assertEquals(set5.contains("A"), true);
+		assertEquals(set5.contains("C"), true);
+	}
+	
+	@Test
+	public void testRemove() {
+		SetADT<String> set6 = new JavaSet<>();
+		set6.insert("A");
+		set6.insert("C");
+		set6.remove("A");
+		assertEquals(set6.contains("A"), false);
+		assertEquals(set6.contains("C"), true);
+		assertEquals(set6.size(), 1);
+	}
+	
+	@Test
+	public void testRemoveNot() {
+		SetADT<String> set6 = new JavaSet<>();
+		set6.insert("A");
+		set6.insert("C");
+		set6.remove("Z");
+		assertEquals(set6.size(), 2);
+		assertEquals(set6.contains("A"), true);
+		assertEquals(set6.contains("C"), true);
+		assertEquals(set6.contains("Z"), false);
+	}
+	
 }
